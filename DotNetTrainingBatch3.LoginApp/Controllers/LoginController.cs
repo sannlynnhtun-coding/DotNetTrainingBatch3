1d15534d39e133de8977dfb9aa713d8295863afc
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-04-06 23:07:19 +0630

    -

--- DotNetTrainingBatch3.LoginApp/Controllers/LoginController.cs
+++ DotNetTrainingBatch3.LoginApp/Controllers/LoginController.cs
@@ -16,6 +16,9 @@ namespace DotNetTrainingBatch3.LoginApp.Controllers
         [ActionName("Index")]
         public IActionResult LoginIndex(LoginModel reqModel)
         {
+            string id = Guid.NewGuid().ToString();
+            string userId = Ulid.NewUlid().ToString();
+
             CookieOptions option = new CookieOptions();
             option.Expires = DateTime.Now.AddSeconds(10);
             Response.Cookies.Append("UserName", reqModel.UserName, option);
@@ -31,5 +34,14 @@ namespace DotNetTrainingBatch3.LoginApp.Controllers
         // UserId
         // SessionId
         // SessionExpired
+
+        // Create Form 
+        // User (UserName, Password) 
+
+        // check user exist
+        // UserId
+        // New Session Id - guid / ulid
+        // SessionExpired
+        // Tbl_Login
     }
 }
